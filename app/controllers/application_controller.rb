9ebabdc986e813bd660e81074c0bf4da6ea6c9b4
miikousin <daks.0817@icloud.com>
2024-05-11 01:00:29 +0900

    新規会員登録機能を実装した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_parmitted_parameters, if: :devise_controller?
+
+  private
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kana, :last_kana, :birth_day])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
+         validates :email, presence: true
          validates :first_name, presence: true
          validates :last_name, presence: true      
          validates :first_kana, presence: true
