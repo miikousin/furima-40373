6e77e2dd15a49a0d434f725f73216a27bfe6f380
miikousin <daks.0817@icloud.com>
2024-05-11 23:53:10 +0900

    バリデーションの設定をしてユーザー管理機能を完成した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,7 +7,8 @@ class User < ApplicationRecord
          validates :email, presence: true
          validates :first_name, presence: true
          validates :last_name, presence: true      
-         validates :first_kana, presence: true
+         validates :first_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "は全角カタカナで入力して下さい。" }
+         validates :last_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "は全角カタカナで入力して下さい。" }
          validates :last_kana, presence: true
          validates :birth_day, presence: true
 end
