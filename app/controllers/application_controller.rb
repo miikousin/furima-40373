e4f79cd57e4bb8aeeec6ff500c8ab84ccd2d08ad
miikousin <daks.0817@icloud.com>
2024-05-11 23:23:25 +0900

    ログイン・ログアウトを実装した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :configure_parmitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path 
+  end
 
   private
   def configure_parmitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kana, :last_kana, :birth_day])
   end
 end
+
