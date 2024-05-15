b5b46f7002333b36201668830e15fe1a06086b3a
miikousin <daks.0817@icloud.com>
2024-05-15 21:46:58 +0900

    application_controller.rbの不要な記術を削除した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_parmitted_parameters, if: :devise_controller?
 
-  def after_sign_out_path_for(resource_or_scope)
-    root_path 
-  end
-
   private
   def configure_parmitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kana, :last_kana, :birth_day])
