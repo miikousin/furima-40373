27908cc59398a0dcec015ae8b9f7ae48c3d3d6e5
miikousin <daks.0817@icloud.com>
2024-05-17 00:01:56 +0900

    active_hashを導入してアソシエーションを定義した

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :number_of_day
+  belongs_to :category
+  
   belongs_to :user
   has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_15_161707) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_15_180447) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_15_161707) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "condition_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
