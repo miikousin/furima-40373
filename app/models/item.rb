0f854202f19f251e750ececece7a9b11f0ec8421
miikousin <daks.0817@icloud.com>
2024-05-17 22:38:11 +0900

    出品ボタンから商品出品ページへ遷移するようにした

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,8 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :condition
-  belongs_to :postage
-  belongs_to :region
-  belongs_to :number_of_day
-  belongs_to :category
   
   belongs_to :user
   has_one_attached :image
+
+  validates :name, :price, :items_description, presence: true
+  validates :category_id, :postage_id, :condition_id, :region_id, :number_of_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
