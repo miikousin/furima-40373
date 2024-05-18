c7658a1e4edd13b37721a87358b80f8a73759521
miikousin <daks.0817@icloud.com>
2024-05-18 23:46:16 +0900

    設定したアクティブハッシュがビューで表示されるようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,11 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
   def new
     @item = Item.new
   end
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -13,7 +15,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:name, :price, :items_description, :category_id, :postage_id, :region_id, :number_of_day_id, :image)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :number_of_day
+  belongs_to :category
   
   belongs_to :user
   has_one_attached :image
 
-  validates :name, :price, :items_description, presence: true
+  validates :name, :items_description, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ }
   validates :category_id, :postage_id, :condition_id, :region_id, :number_of_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
