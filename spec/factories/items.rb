eaf10574a85f3bccc7ea5fc35a8e2985a1728397
miikousin <daks.0817@icloud.com>
2024-05-19 00:21:18 +0900

    item_spec.rbを作成した

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name               { Faker::Commerce.product_name }
+    price              { Faker::Number.within(range: 300..9999999) }
+    items_description  { Faker::Lorem.sentence }
+    category_id        { Faker::Number.within(range: 2..11)}
+    postage_id         { Faker::Number.within(range: 2..3)}
+    condition_id       { Faker::Number.within(range: 2..7)}
+    region_id          { Faker::Number.within(range: 2..48)}
+    number_of_day_id   { Faker::Number.within(range: 2..4)}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it 'name、price、items_description、category_id、postage_id、condition_id、region_id、number_of_day_idがあれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
 end
