f4f0a411663ef6628ed72083f3cecdcf7b5f4bc0
miikousin <daks.0817@icloud.com>
2024-06-11 16:00:26 +0900

    テストコードを追加した

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -96,6 +96,12 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Item can't be blank")
       end
+
+      it 'tokenが空では購入できない' do
+        @order_form.token = ""  
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
