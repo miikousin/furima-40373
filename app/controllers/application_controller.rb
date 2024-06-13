62744bc23a987bc18e372c38c7bed8f1b715516d
miikousin <daks.0817@icloud.com>
2024-06-13 21:37:08 +0900

    Basic認証を変更した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :basic_auth
   before_action :configure_parmitted_parameters, if: :devise_controller?
 
   private
   def configure_parmitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kana, :last_kana, :birth_day])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
 
