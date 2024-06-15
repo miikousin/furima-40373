a0f52e0d1adf25a23c410f0342a484b7eb108efc
miikousin <daks.0817@icloud.com>
2024-06-15 21:51:18 +0900

    item-price.jsとビューファイルを修正した

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,4 @@
-window.addEventListener("turbo:load", () => {
-  //販売手数料と利益の計算を関数(calculateFeeAndProfit)として定義
+//販売手数料と利益の計算を関数(calculateFeeAndProfit)として定義
 function calculateFeeAndProfit() {
   const itemPrice = document.getElementById("item-price").value;//販売価格の入力値を取得、itemPriceに代入
   const fee = Math.floor(itemPrice / 10);//手数料。itemPriceの数値の10％を小数点以下を切り捨(Math.floor)、feeに代入
@@ -10,10 +9,20 @@ function calculateFeeAndProfit() {
 
   const profitDom = document.getElementById("profit");//販売利益を表示する場所を取得
   profitDom.innerHTML = profit;//販売利益のHTMLをprofitで更新
+  console.log("calculateFeeAndProfit function has been called");
 }
-const itemPriceElement = document.getElementById("item-price");
 
+function addInputEventListener() {
+  const itemPriceElement = document.getElementById("item-price");
+   
   if (itemPriceElement != null) {
     itemPriceElement.addEventListener("input", calculateFeeAndProfit);
   }
-})
+}
+
+window.addEventListener("turbo:load", addInputEventListener);
+document.addEventListener('DOMContentLoaded', calculateFeeAndProfit);
+
+document.addEventListener("turbo:load", calculateFeeAndProfit);
+document.addEventListener("turbo:render", calculateFeeAndProfit);
+
